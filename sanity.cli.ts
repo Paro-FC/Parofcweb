13b7145e20f544b1f783771e7d970df0a0d1ec2f
ChimiGit <ctsheringgit208@gmail.com>
2026-02-09 18:13:39 +0600

    fixed

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '4rd3jbsr',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   },
   studioHost: 'parofc',
 })
